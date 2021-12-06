3afe3111be659b1d00e0e2aa565f80463a913f67
sophiacofone <95585108+sophiacofone@users.noreply.github.com>
2021-12-05 19:19:27 -0500

    Add files via upload

---
+++ finalproject5002.py
@@ -0,0 +1,75 @@
+#Sophia Cofone, 12.5.21, CS5002, file is intented to be an application of bayes' rule using spotify genre data for final cs5002 project
+
+import pandas as pd
+
+file = 'spotify_genre_final.xlsx'
+#imports the spotify data file, change to the name of your file or change your file name to this
+cols = ["Genre","danceability"]
+#specifies which columns in the data file we care about for the data frame (table)
+df = pd.read_excel(file, index_col=None)
+#reads the excel file into a dataframe
+df2 = pd.DataFrame(df[cols])
+#makes a new data frame with the columns we care about (this will be the data frame used going fwd)
+df2['highdance'] = df2['danceability'] >= .7
+#this adds a new column named "highdance" to the dataframe. It is a boolean value that contains "true" if the song has a dancability rating of .7 or higher
+#note this is arbitrary, I just selected .7 becuase it was about half the songs (half the songs were either higher or lower than .7)
+
+def prior(df):
+    '''Function: The function prior calculates P(A)...the probability of a particular song having a danceability rating 0.7 or above (total library of songs) 
+    Parameters: a dataframe (df2)
+    Return: float, P(A)'''
+    numsongs = df.shape[0]
+    num_dancable_songs = sum(df['highdance'])
+    p_a = num_dancable_songs/numsongs
+
+    return p_a
+
+def likelihood(genre,df):
+    '''Function: The function likelihood calculates P(B|A)...the probability of a song being in x (EDM, pop, etc.) genre given that it has a danceability rating 0.7 or higher
+    Parameters: genre(str), a dataframe (df2)
+    Return: float, P(B|A)'''
+    df_songs_dance = df[df.danceability >=0.7]
+    a = sum(df_songs_dance.Genre == genre)      # number of songs that are genre (ex. 'rock') and dancible
+    y = sum(df['highdance'])                    # number of danceable songs
+    p_b_a = a/y
+    
+    return p_b_a
+
+def evidence(genre,df):
+    '''Function: The function evidence calculates P(B)...the probability of a song being in x (EDM, pop, etc.) genre
+    Parameters: genre(str), a dataframe (df2)
+    Return: float, P(B)'''
+    numsongs = df.shape[0]
+    num_dancable_songs = sum(df['highdance'])
+    p_a = num_dancable_songs/numsongs
+    
+    df_songs_dance = df[df.danceability >=0.7]
+    a = sum(df_songs_dance.Genre == genre)      # number of songs that are genre (ex. 'rock') and dancible
+    y = sum(df['highdance'])                    # number of danceable songs
+    p_b_a = a/y
+    
+    df_songs_no_dance = df[df.danceability < 0.7]
+    c = sum(df_songs_no_dance.Genre == genre)   # number of songs that are genre (ex. 'rock') and not dancible
+    x = sum(df['highdance'] ==False)            # number of not danceable songs
+    
+    p_b_not_a = c/x
+    
+    p_b = p_b_a*p_a+p_b_not_a*(1-(p_a)) 
+    
+    return p_b
+
+def bayes(genre,df):
+    '''Function: The function bayes (posterior) calculates P(A|B)...the probability of a particular song having a danceability rating 0.7 or above given the fact that it is in the x (EDM, pop, etc.) genre.
+    Parameters: genre(str), a dataframe (df2)
+    Return: float, P(A|B)'''
+    p_a_b = ((likelihood(genre,df)) * (prior(df))) / (evidence(genre,df))
+    
+    return p_a_b
+
+def main(): 
+    print("Probability of any song in your Spotify library being highly dancable",(round((prior(df2))*100,0)),"%")
+    genre = str(input("Please enter a Spotify music genre from this list: rock, edm, pop, hiphop, latin, r&b, rap: "))
+    print("Probability of any song in your Spotify library being highly dancable GIVEN the genre is",genre,(round((bayes(genre,df2))*100,0)),"%")
+
+main()
+
